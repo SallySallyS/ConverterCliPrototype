b88059b9537e458a7f74b1d57d11acbae4a5f0bd
SallySallyS <sova.lizochka2006@gmail.com>
2025-12-09 10:38:05 +0300

    добавила опции и поверки

--- main.cpp
+++ main.cpp
@@ -1,7 +1,59 @@
 #include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QDebug>
+#include <QFileInfo>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
-    return a.exec();
+    QCoreApplication app(argc, argv);
+    app.setApplicationName("converter");
+    app.setApplicationVersion("1.0");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("CAD file converter");
+
+    // Обязательные опции
+    QCommandLineOption inputOption("i", "Input file", "file");
+    QCommandLineOption outputOption("o", "Output file", "file");
+
+
+    QCommandLineOption linearOption("l", "Linear precision", "value", "0.1");
+    QCommandLineOption angularOption("a", "Angular precision", "value", "0.3");
+
+    parser.addOption(inputOption);
+    parser.addOption(outputOption);
+    parser.addOption(linearOption);
+    parser.addOption(angularOption);
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.process(app);
+
+    // Проверка обязательных аргументов
+    if (!parser.isSet(inputOption) || !parser.isSet(outputOption)) {
+        qCritical() << "Error: Input and output files required";
+        parser.showHelp(1);
+    }
+
+    QString inputFile = parser.value(inputOption);
+    QString outputFile = parser.value(outputOption);
+    double linearPrecision = parser.value(linearOption).toDouble();
+    double angularPrecision = parser.value(angularOption).toDouble();
+
+    // Проверка файлов
+    if (!QFileInfo::exists(inputFile)) {
+        qCritical() << "Error: Input file not found";
+        return 1;
+    }
+
+    // Вывод параметров
+    qInfo() << "Converting:" << inputFile << "->" << outputFile;
+    qInfo() << "Linear precision:" << linearPrecision;
+    qInfo() << "Angular precision:" << angularPrecision;
+
+    // Здесь будет вызов конвертера
+    // convert(inputFile, outputFile, linearPrecision, angularPrecision);
+
+    qInfo() << "Conversion complete";
+    return 0;
 }
